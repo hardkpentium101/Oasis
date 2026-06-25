57cfd6322cd307af0d36b9694cfa856e42995740
hardkpentium101 <hardkpentium101@gmail.com>
2026-06-26 01:45:21 +0530

    fix input for rwsbrk and usertests.c

--- user/usertests.c
+++ user/usertests.c
@@ -241,7 +241,7 @@ copyinstr3(char *s)
 // See if the kernel refuses to read/write user memory that the
 // application doesn't have anymore, because it returned it.
 void
-rwsbrk()
+rwsbrk(char *s)
 {
   int fd, n;
   
@@ -2380,7 +2380,7 @@ fsfull()
     nfiles--;
   }
 
-  printf("fsfull test finished\n");
+  printf("fsfull test finished (fsblocks: %d)\n", fsblocks);
 }
 
 void argptest(char *s)
