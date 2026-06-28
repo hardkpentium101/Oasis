ed0827e015841e38dbdc1894a234cf31f0a9d034
hardkpentium101 <hardkpentium101@gmail.com>
2026-06-28 17:38:42 +0530

    update uprogs add sleep, fix sleep.c

--- user/sleep.c
+++ user/sleep.c
@@ -1,23 +1,15 @@
-#include "kernel/defs.h"
-#include "kernel/proc.h"
-#include "kernel/syscall.c"
 #include "kernel/types.h"
 
 #include "user/user.h"
 
-extern uint64 sys_sleep();
-extern struct proc *myproc();
-int sleep(int n) {
-
-  myproc()->trapframe->a0 = n;
-  sys_sleep();
-
-  return 0;
-}
-
 int main(int argc, char *argv[]) {
 
-  sleep(argv[1]);
+  if (argc == 1) {
+    fprintf(2, "Arg required...");
+    exit(1);
+  }
+
+  sleep(atoi(argv[1]));
 
   exit(0);
 }
